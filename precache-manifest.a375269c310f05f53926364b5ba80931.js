fea221153ebdfa446a304a93f34e00d14c13303f
Rodrigo Castro <rodrigocastro@iMac.lan>
2020-06-13 17:28:14 +0200

    Updates

---
+++ precache-manifest.a375269c310f05f53926364b5ba80931.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f9074deef0b4a06a479e95e539da6e",
+    "url": "/germancats/index.html"
+  },
+  {
+    "revision": "b0cba9f4efbbe4e85fd1",
+    "url": "/germancats/static/css/main.7bd431dc.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/germancats/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/germancats/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0cba9f4efbbe4e85fd1",
+    "url": "/germancats/static/js/main.f540665e.chunk.js"
+  },
+  {
+    "revision": "2c43632db8b141705e89",
+    "url": "/germancats/static/js/runtime-main.0f3a55f4.js"
+  }
+]);
